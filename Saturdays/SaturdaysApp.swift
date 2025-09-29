6601929cb895905c22d7fda5dd7fa7f4bbb4db58
Rosemary Yang <yang.rosemary.e@gmail.com>
2025-09-29 17:23:15 -0400

    Initial Commit

---
+++ Saturdays/SaturdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaturdaysApp.swift
+//  Saturdays
+//
+//  Created by Rosemary Yang on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SaturdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
