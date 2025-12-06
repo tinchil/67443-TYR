f03acce425280a13eef2ed5d335f507a5c373396
Rosemary Yang <yang.rosemary.e@gmail.com>
2025-12-06 06:35:22 -0500

    added pixel buffering tests'

---
+++ SaturdaysTests/PixelBufferRendererTests.swift
@@ -0,0 +1,46 @@
+import Testing
+import UIKit
+@testable import Saturdays
+
+struct PixelBufferRendererTests {
+
+    @Test
+    func testCreatesNonNilPixelBuffer() throws {
+        let image = UIImage(systemName: "star")!
+        let size = CGSize(width: 300, height: 500)
+
+        let buffer = PixelBufferRenderer.createPixelBuffer(from: image, size: size)
+
+        #expect(buffer != nil)
+    }
+
+    @Test
+    func testPixelBufferHasCorrectDimensions() throws {
+        let image = UIImage(systemName: "star")!
+        let size = CGSize(width: 400, height: 600)
+
+        let buffer = try #require(
+            PixelBufferRenderer.createPixelBuffer(from: image, size: size)
+        )
+
+        let width = CVPixelBufferGetWidth(buffer)
+        let height = CVPixelBufferGetHeight(buffer)
+
+        #expect(width == Int(size.width))
+        #expect(height == Int(size.height))
+    }
+
+    @Test
+    func testPixelBufferIsARGBFormat() throws {
+        let image = UIImage(systemName: "star")!
+        let size = CGSize(width: 200, height: 200)
+
+        let buffer = try #require(
+            PixelBufferRenderer.createPixelBuffer(from: image, size: size)
+        )
+
+        let format = CVPixelBufferGetPixelFormatType(buffer)
+
+        #expect(format == kCVPixelFormatType_32ARGB)
+    }
+}
